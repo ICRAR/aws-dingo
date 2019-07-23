47589bc87c4129d84dc4ea0e3ff7c5403a46f5d0
KevinVinsen <kvn1961@gmail.com>
2019-07-23 10:43:37 +0200

    Adding c5.12xlarge

---
+++ aws/parallel-cluster/dingo-g12-test-c5.12xlarge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Number of MPI tasks
+#SBATCH -n 244
+#
+# Number of tasks per node
+#SBATCH --tasks-per-node=30
+
+# c5.12xlarge - 48 cores 96 GB RAM
+
+module load openmpi/3.1.4
+cd /fsx/G12_test
+#mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in -l askap.log_cfg
+mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in
