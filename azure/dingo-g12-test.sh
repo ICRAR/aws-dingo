8337d36f391d129ee22906cb7b0a2384cc892b34
Kevin Vinsen <kvn1961@gmail.com>
2019-08-26 11:29:15 +0800

    Move to srun for test

--- azure/dingo-g12-test.sh
+++ azure/dingo-g12-test.sh
@@ -10,6 +10,6 @@ cd /lustre/G12_test
 #mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in -l askap.log_cfg
 #mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in
 
-NCORES=244
-NPPN=25
-srun --export=ALL --ntasks=244 --ntasks-per-node=25 imager -p -c science_spectral_imager_F00_B00_5575077.in
+#NCORES=244
+#NPPN=25
+srun --export=ALL --no-kill --ntasks=244 --ntasks-per-node=25 imager -p -c science_spectral_imager_F00_B00_5575077.in
