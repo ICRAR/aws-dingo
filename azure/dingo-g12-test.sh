163987c35ac83500f111beb9e5a14e9b4191f6c6
Kevin Vinsen <kvn1961@gmail.com>
2019-08-27 15:00:42 +0800

    Move to srun for test

--- azure/dingo-g12-test.sh
+++ azure/dingo-g12-test.sh
@@ -8,8 +8,8 @@ module load mpi/openmpi-x86_64
 
 cd /lustre/G12_test
 #mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in -l askap.log_cfg
-#mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in
+mpirun -np 244 imager -p -c science_spectral_imager_F00_B00_5575077.in
 
 #NCORES=244
 #NPPN=25
-srun --export=ALL --no-kill --ntasks=244 --ntasks-per-node=25 imager -p -c science_spectral_imager_F00_B00_5575077.in
+#srun --export=ALL --no-kill --ntasks=244 --ntasks-per-node=25 imager -p -c science_spectral_imager_F00_B00_5575077.in
